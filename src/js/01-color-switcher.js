5c423831a951989e205e0eb8f16cade0ba933484
Oleksandr <oleksandr@burtovyi.com>
2023-11-14 11:29:47 +0100

    task #1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
+let timerBgColor = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function startTimerChangeColor() {
+  timerBgColor = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+}
+
+function stopChangeColor() {
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+  clearInterval(timerBgColor);
+}
+refs.btnStart.addEventListener('click', startTimerChangeColor);
+refs.btnStop.addEventListener('click', stopChangeColor);
